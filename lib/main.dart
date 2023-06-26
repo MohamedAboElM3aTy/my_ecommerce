a36b3d1bbcc47ab4e1eda1beec74b183ecc80c78
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-06-26 05:02:47 +0300

    Wrapping myApp with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:my_ecommerce/utilities/router.dart';
 import 'package:my_ecommerce/utilities/routes.dart';
 import 'package:my_ecommerce/utilities/theme_light.dart';
+import 'package:my_ecommerce/services/firebase_options.dart';
 
-void main() => runApp(
-      const MyApp(),
-    );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const MyApp(),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
