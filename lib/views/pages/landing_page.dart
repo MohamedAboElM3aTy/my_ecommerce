668ed1ca57522bb773a814b62bb36ef2078edc16
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-07-08 00:26:42 +0300

    Move the logic to landing page

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+
+import 'package:my_ecommerce/controllers/auth_controller.dart';
+import 'package:my_ecommerce/views/pages/bottom_nav_bar.dart';
+import 'package:my_ecommerce/views/pages/auth_page.dart';
+import 'package:my_ecommerce/services/auth.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return StreamBuilder<User?>(
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          if (user == null) {
+            return ChangeNotifierProvider<AuthController>(
+              create: (_) => AuthController(auth: auth),
+              child: const AuthPage(),
+            );
+          }
+          return ChangeNotifierProvider<AuthController>(
+            create: (_) => AuthController(auth: auth),
+            child: const BottomNavBar(),
+          );
+        }
+
+        return const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator.adaptive(),
+          ),
+        );
+      },
+    );
   }
 }
