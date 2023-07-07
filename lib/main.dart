31c28c98730aedc170974b98c706049333b175fe
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-07-08 00:20:51 +0300

    Wrapping with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'package:my_ecommerce/utilities/theme_light.dart';
 import 'package:my_ecommerce/utilities/router.dart';
 import 'package:my_ecommerce/utilities/routes.dart';
-import 'package:my_ecommerce/utilities/theme_light.dart';
+import 'package:my_ecommerce/services/auth.dart';
 import 'package:my_ecommerce/services/firebase_options.dart';
 
 void main() async {
@@ -21,12 +23,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: myTheme(),
-      title: 'Ecommerce App',
-      initialRoute: AppRoutes.authPageRoute,
-      onGenerateRoute: generateRoute,
+    return Provider<AuthBase>(
+      create: (_) => Auth(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: myTheme(),
+        title: 'Ecommerce App',
+        initialRoute: AppRoutes.landingPageRoute,
+        onGenerateRoute: generateRoute,
+      ),
     );
   }
 }
